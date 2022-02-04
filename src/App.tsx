26adc88acceb492aed113b384fdf504331e421d2
amagaitomohiro <amagaipps@gmail.com>
2022-02-04 15:27:35 +0900

    途中

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Bank from './pages/bank/Bank';
+
+function App() {
+  return (
+    <div>
+      <h2>銀行アプリ</h2>
+      <Bank />
+    </div>
+)};
+
+export default App;
---
+++ src/pages/bank/Bank.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useReducer } from 'react';
+import styled from 'styled-components';
+
+type State = {
+    savings: number;
+    histories: {operation: string; amount: number}[];
+};
+
+type Action = 
+  | {type: 'DEPOSIT'; payload: number }
+  | {type: 'WITHDRAW'; payload: number };
+
+const initialState = {
+  savings: 0,
+  histories: [],
+}
+
+const reducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case 'DEPOSIT':
+            return {
+            ...state, 
+            savings: state.savings + action.payload,
+            histories: [
+                ...state.histories,
+                { operation: '預け入れ', amount: action.payload },
+            ],
+        };
+        case 'WITHDRAW':
+            return {...state, savings: state.savings - action.payload,
+            histories: [
+                ...state.histories,
+                { operation: '引き出し', amount: action.payload },
+            ],
+            };
+        default: 
+          return state;
+    }
+}
+
+const Bank: React.VFC = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [amount, setAmount] = useState<number>(0);
+
+    const onDeposit = (amount: number) => {
+      dispatch({ type: 'DEPOSIT', payload: amount });
+    }
+    const onWithdraw = (amount: number) => {
+      dispatch({ type: 'WITHDRAW', payload: amount });
+    }
+    return (
+      <div>
+        <h4>残高: {state.savings.toLocaleString()}円</h4>
+        <div>
+          <input 
+          value={amount} 
+          onChange={(e) => {
+            setAmount(Number(e.target.value));
+          }}
+          type='number'
+          />
+          <span>円</span>
+        </div>
+        <div>
+          <button onClick={() => onDeposit(amount)}>預け入れ</button>
+          <button onClick={() => onWithdraw(amount)}>引き出し</button>
+        </div>
+        <div>
+          <h4>履歴</h4>
+          {state.histories.map((history, index) => {
+              return(
+                <p key={index}>
+                  {history.operation}:{history.amount.toLocaleString()}
+                </p>
+              )
+          })}
+        </div>
+      </div>
+  );
+};
+
+// const Input = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+
+//   span {
+//       margin-left: 4px;
+//   }
+// `
+
+// const Button = styled.button`
+//   display: flex;
+//   margin-top: 8px;
+
+//   button {
+//       margin-left: 4px;
+//       margin-right: 4px;
+//   }
+// `
+
+export default Bank
